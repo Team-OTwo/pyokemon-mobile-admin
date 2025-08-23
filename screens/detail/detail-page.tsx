f45a8cbd47f2203c543656a258d93bcfe6949aee
sky121016 <sky121016@naver.com>
2025-08-23 14:59:30 +0900

    [Feature/#10] 공연 상세 디자인 수정

--- screens/detail/detail-page.tsx
+++ screens/detail/detail-page.tsx
@@ -1,5 +1,6 @@
 import { useGetEventDetailQuery } from "@/api/event/queries/use-get-event-detail-query";
-import Header from "@/components/header";
+import HeaderTransparent from "@/components/header-transparent";
+import Badge from "@/components/ui/badge";
 import CustomButton from "@/components/ui/button";
 import Loading from "@/components/ui/loading";
 import { Colors } from "@/constants/Colors";
@@ -10,7 +11,7 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 type DetailPageProps = {
   navigation: NativeStackNavigationProp<RootStackParamList, "Detail">;
@@ -26,10 +27,6 @@ function DetailPage({ route, navigation }: DetailPageProps) {
     error,
   } = useGetEventDetailQuery(Number(eventId));
 
-  const handleLogPress = () => {
-    navigation.navigate("ScanLog", { eventId });
-  };
-
   const handleQrButtonPress = () => {
     navigation.navigate("ChallengeQr");
   };
@@ -40,12 +37,15 @@ function DetailPage({ route, navigation }: DetailPageProps) {
 
   return (
     <View style={styles.screen}>
-      <Header title="공연 상세" />
+      <HeaderTransparent/>
 
+      <View><Image source={{uri: event.thumbnailUrl}} style={styles.image}></Image></View>
       {/* content */}
       <View style={styles.container}>
+        <Badge text={event.genre} backgroundColor={Colors.primary20} />
         {/* title */}
-        <Text style={[styles.title, globalStyles.title]}>{event?.title}</Text>
+        <Text style={[styles.title, globalStyles.title]}>{event.title}</Text>
+
 
         {/* 일시, 장소 */}
         <View style={styles.infoContainer}>
@@ -60,8 +60,14 @@ function DetailPage({ route, navigation }: DetailPageProps) {
 
           <View style={styles.info}>
             <Text style={styles.infoTitle}>장소</Text>
-            <Text style={styles.infoText}>{event?.venueName}</Text>
+            <Text style={styles.infoText}>{event.venueName}</Text>
           </View>
+          
+          <View style={styles.info}>
+            <Text style={styles.infoTitle}>연령</Text>
+            <Text style={styles.infoText}>{event.ageLimit === 0 ? "전체 관람가" : event.ageLimit + "세"}</Text>
+          </View>
+          
         </View>
       </View>
       <View style={styles.qrButton}>
@@ -74,6 +80,7 @@ function DetailPage({ route, navigation }: DetailPageProps) {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    position:'relative'
   },
   container: {
     padding: 16,
@@ -84,6 +91,7 @@ const styles = StyleSheet.create({
   },
   title: {
     marginBottom: 24,
+    marginTop:8,
     borderBottomColor: Colors.gray100,
   },
   infoContainer: {
@@ -117,6 +125,10 @@ const styles = StyleSheet.create({
     gap: 12,
     width: "100%",
   },
+  image:{
+    width:'100%',
+    height:360,
+  }
 });
 
 export default DetailPage;
